9bb11c7dfef30bad72cea364e546be1c65f149f8
miguel <m.miguelvasquez11@gmail.com>
2022-08-02 11:48:32 -0500

    Trabajando con sets y objetos exercise 07.2

--- exercise07.2objetos.js
+++ exercise07.2objetos.js
@@ -11,5 +11,5 @@ const datos_personales = [{...objets},{name: "Maria",lastname: "Gonzales",age: 2
 console.log(datos_personales);
 
 //Una nueva lista con los objetos de la lista anterior ordenados por edad, de mayor a menor
-const listaordenada = datos_personales.sort((a,b)=>a.age-b.age)
+const listaordenada = datos_personales.sort((a,b)=>b.age-a.age)
 console.log(listaordenada);
