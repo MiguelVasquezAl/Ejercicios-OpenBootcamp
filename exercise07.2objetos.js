51aac845793e0f6de240c89a912d64713cae2c9c
miguel <m.miguelvasquez11@gmail.com>
2022-08-02 11:47:06 -0500

    Trabajando con sets y objetos exercise 07.2

--- exercise07.2objetos.js
+++ exercise07.2objetos.js
@@ -7,7 +7,7 @@ const myage = objets.age
 console.log(myage);
 
 //Una lista que contenga el objeto con tus datos personales y un nuevo objeto con los datos personales de tus dos mejores amig@s
-const datos_personales = [objets,{name: "Maria",lastname: "Gonzales",age: 23,height: 1.57,isDeveloper: true},{name: "Pamela",lastname: "Miranda",age: 28,height: 1.59,isDeveloper: false}]
+const datos_personales = [{...objets},{name: "Maria",lastname: "Gonzales",age: 23,height: 1.57,isDeveloper: true},{name: "Pamela",lastname: "Miranda",age: 28,height: 1.59,isDeveloper: false}]
 console.log(datos_personales);
 
 //Una nueva lista con los objetos de la lista anterior ordenados por edad, de mayor a menor
