53c8a4a3ce546ba1da2c9de7d863d08a36e371e3
miguel <m.miguelvasquez11@gmail.com>
2022-08-01 22:04:28 -0500

    Trabajando con sets y objetos exercise 07.1

--- exercise07.1conjuntos.js
+++ exercise07.1conjuntos.js
@@ -6,7 +6,9 @@ const family = new Set(listfamily)
 console.log(family);
 
 // Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+family.add("Miguel")
 console.log(family.add("Miguel"));
 
 //Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;)
+family.add("JavaScript")
 console.log(family.add("JavaScript"));
