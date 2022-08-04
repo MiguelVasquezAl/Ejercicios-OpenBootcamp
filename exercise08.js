0cddf67ef58ab6289d55da858bf9a080258c1629
miguel <m.miguelvasquez11@gmail.com>
2022-08-04 16:22:56 -0500

    Introducción a las funciones exercise 07

---
+++ exercise08.js
@@ -0,0 +1,34 @@
+//Una función sin parámetros que devuelva siempre "true"
+function n() {
+    return true
+}
+console.log(n());
+
+
+//Una función asíncrona que utilice un setTimeout y pase por consola un "Hola soy una promesa" 5 segundos después de haberse ejecutado
+const mipromesa = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        resolve("Hola soy una promesa"),5000
+    })
+})
+mipromesa
+
+// Una función generadora de índices pares automáticos
+function* pares() {
+    let i = 0
+    while (true) {
+        i++
+        if(i%2===0) yield i
+    }
+}
+const exit = pares ()
+console.log(exit.next());
+console.log(exit.next());
+console.log(exit.next());
+console.log(exit.next());
+console.log(exit.next());
+console.log(exit.next());
+console.log(exit.next());
+console.log(exit.next());
+console.log(exit.next());
+
