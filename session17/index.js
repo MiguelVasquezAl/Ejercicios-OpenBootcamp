15417ec30274c261d71133f3ce51faba4e18935c
miguel <m.miguelvasquez11@gmail.com>
2022-09-13 14:56:43 -0500

    exercise17

--- session17/index.js
+++ session17/index.js
@@ -1,12 +1,44 @@
+let markers, map;
+
 function initMap() {
-  const uluru = { lat: -25.344, lng: 131.031 };
-  const map = new google.maps.Map(document.getElementById("map"), {
+  const posicion = {
+    lat: -25.363,
+    lng: 131.044,
+  };
+
+  map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
-    center: uluru,
+    center: posicion,
   });
-  const marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
-  });
+
+  markers.push(
+    new google.maps.Marker({
+      position: {
+        lat: 43.288773445332524,
+        lng: -2.163845628766325,
+      },
+      map,
+      title: "Zarautz",
+    })
+  );
+  markers.push(
+    new google.maps.Marker({
+      position: {
+        lat: 36.164983935422484,
+        lng: -86.78260332112534,
+      },
+      map,
+      title: "Nashville",
+    })
+  );
+  markers.push(
+    new google.maps.Marker({
+      position: {
+        lat: 36.242150706885596,
+        lng: -6.0775131009957715,
+      },
+      map,
+      title: "El Palmar",
+    })
+  );
 }
-// window.initMap = initMap;
