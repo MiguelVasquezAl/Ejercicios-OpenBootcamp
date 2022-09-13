233fe33637abe840f6df017d83c15bf9120ca6bc
miguel <m.miguelvasquez11@gmail.com>
2022-09-13 13:19:17 -0500

    exercise16

--- session16/index.js
+++ session16/index.js
@@ -3,9 +3,11 @@ const secciones = document.querySelectorAll(".seccion");
 
 parrafos.forEach((parrafo) => {
   parrafo.addEventListener("dragstart", (event) => {
+    console.log("Estoy arrastrando el párrafo: " + parrafo.innerText);
     parrafo.classList.add("dragging");
-    const elemento_fantasma = document.createElement("div");
-    event.dataTransfer.dragImage(elemento_fantasma, 0, 0);
+    event.dataTransfer.setData("id", parrafo.id);
+    const elemento_fantasma = document.querySelector(".imagen-fantasma");
+    event.dataTransfer.setDragImage(elemento_fantasma, 0, 0);
   });
   parrafo.addEventListener("dragend", () => {
     parrafo.classList.remove("dragging");
@@ -13,9 +15,25 @@ parrafos.forEach((parrafo) => {
 });
 
 secciones.forEach((seccion) => {
-  seccion.addEventListener("dragover", (e) => {
-    e.preventDefault();
-    const parrafo = document.querySelector(".dragging");
+  seccion.addEventListener("dragover", (event) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "copy";
+  });
+  seccion.addEventListener("drop", (event) => {
+    console.log("Drop");
+    const id_parrafo = event.dataTransfer.getData("id");
+    const parrafo = document.getElementById(id_parrafo);
     seccion.appendChild(parrafo);
   });
+});
+const papelera = document.querySelector(".papelera");
+
+papelera.addEventListener("dragover", (event) => {
+  event.preventDefault();
+  event.dataTransfer.dropEffect = "copy";
+});
+
+papelera.addEventListener("drop", (event) => {
+  const id_parrafo = event.dataTransfer.getData("id");
+  document.getElementById(id_parrafo).remove();
 });
